80fcbb36f115ec40952b34b961aaea1f732cb337
nunezada08 <vitor.nunes3013@gmail.com>
2025-09-12 21:34:39 -0300

    feat: faz script da pagina de login

---
+++ assets/scripts/scriptLogin.js
@@ -0,0 +1,13 @@
+document.getElementById('login').addEventListener('click', function () {
+    const usuario = document.getElementById('usuario').value;
+    const senha = document.getElementById('senha').value;
+
+    const usuarioSalvo = localStorage.getItem('usuario');
+    const senhaSalva = localStorage.getItem('senha');
+
+    if (usuario === usuarioSalvo && senha === senhaSalva) {
+        window.location.href = './index.html'
+    } else {
+        alert('Usuario ou senha estão incorretos!')
+    }
+})
